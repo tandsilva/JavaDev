165c4aa842a2461002f635dca0070d09543e8d9b
thiago.amsilva <tamdsilva@logatti.edu.br>
2023-03-31 19:32:54 -0300

    Taxa de Importacao

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import entities.Product;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        Product product = new Product();
+
+        System.out.println("");
     }
 }
